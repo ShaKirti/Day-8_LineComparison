3eec308b3d8f34166d60bd8d81de30592569b7cd
Shashank Mishra <mishra.shashankofficial06@gmail.com>
2022-10-14 02:42:19 +0530

    Added: Welcome message.

---
+++ src/com/line/comparison/LineComparison.java
@@ -0,0 +1,8 @@
+package com.line.comparison;
+
+public class LineComparison {
+    public static void main(String[] args) {
+
+        WelcomeMessage.msg();
+    }
+}
---
+++ src/com/line/comparison/WelcomeMessage.java
@@ -0,0 +1,7 @@
+package com.line.comparison;
+
+class WelcomeMessage {
+    static void msg(){
+        System.out.println("Welcome to Line Comparison Computation Program.");
+    }
+}
