a0b2d1216a7cf6ea718c167d43c4086fd731e8d5
Shashank Mishra <mishra.shashankofficial06@gmail.com>
2022-10-14 10:58:15 +0530

    Added: Logic to calculate length of a line.

--- src/com/line/comparison/LineComparison.java
+++ src/com/line/comparison/LineComparison.java
@@ -4,5 +4,8 @@ public class LineComparison {
     public static void main(String[] args) {
 
         WelcomeMessage.msg();
+
+        double length = LineCalculation.points(12,12,14,13);
+        System.out.println(length);
     }
 }
--- src/com/line/comparison/WelcomeMessage.java
+++ src/com/line/comparison/WelcomeMessage.java
@@ -1,7 +1,15 @@
 package com.line.comparison;
 
+import java.util.Scanner;
+
 class WelcomeMessage {
     static void msg(){
         System.out.println("Welcome to Line Comparison Computation Program.");
+    }
+}
+class LineCalculation{
+     static double points(int x1, int y1, int x2, int y2){
+        double result = Math.sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
+        return result;
     }
 }
