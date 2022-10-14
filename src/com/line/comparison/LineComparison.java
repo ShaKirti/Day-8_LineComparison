707557a39de9e5f96e73da69ae273e82441c546a
Shashank Mishra <mishra.shashankofficial06@gmail.com>
2022-10-14 11:30:11 +0530

    Added: Logic to compare length of two lines.

--- src/com/line/comparison/LineComparison.java
+++ src/com/line/comparison/LineComparison.java
@@ -6,6 +6,9 @@ public class LineComparison {
         WelcomeMessage.msg();
 
         double length = LineCalculation.points(12,12,14,13);
-        System.out.println(length);
+        double length1 = LineCalculation.pointsNext(15,15,17,18);
+        System.out.println("Length of Line L1: "+length);
+        System.out.println("Length of Line L2 "+length1);
+        LengthComparison.diff(length,length1);
     }
 }
--- src/com/line/comparison/WelcomeMessage.java
+++ src/com/line/comparison/WelcomeMessage.java
@@ -1,7 +1,7 @@
 package com.line.comparison;
 
 import java.util.Scanner;
-
+import java.util.Objects;
 class WelcomeMessage {
     static void msg(){
         System.out.println("Welcome to Line Comparison Computation Program.");
@@ -11,5 +11,17 @@ class LineCalculation{
      static double points(int x1, int y1, int x2, int y2){
         double result = Math.sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
         return result;
+    }
+    static double pointsNext(int x3, int y3, int x4, int y4){
+        double result1 = Math.sqrt(((x4-x3)*(x4-x3))+((y4-y3)*(y4-y3)));
+        return result1;
+    }
+}
+class LengthComparison{
+    static void diff(double result, double result1) {
+        if (Objects.equals(result, result1)) {
+            System.out.println("Both lines are of same length.");
+        } else
+            System.out.println("Both lines are of different length.");
     }
 }
